89a282c466334c5e5e2c422f05f43aeb8d0e7932
mmoreira <mmoreira.dg@gmail.com>
2020-05-19 22:36:32 -0400

    conceitos

--- curso-es6/bundle.js
+++ curso-es6/bundle.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
@@ -80,4 +84,122 @@ var Matematica = /*#__PURE__*/function () {
   return Matematica;
 }();
 
-Matematica.soma(10, 20);
+Matematica.soma(10, 20); //tipo de declaracao de varival nao mutavel
+
+var a = 1; //só é possivel mudar valores de um objeto declarado como const ex.:
+
+var obj = {
+  nome: 'michael'
+};
+obj.nome = 'Douglas';
+console.log(obj); //let - variavel definida somente dentro do escopo ex.: 
+
+function teste(x) {
+  var y = 5;
+
+  if (x > 5) {
+    console.log(x, y);
+  }
+}
+
+teste(10); //arrays
+
+var arr = [1, 3, 4, 8, 9, 10];
+console.log('novo array', arr);
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log('testando o map', newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log('testando o reduce', sum);
+var filter = arr.filter(function (item) {
+  return item % 2 == 0;
+});
+console.log('testando o filter', filter);
+var find = arr.find(function (item) {
+  return item === 4;
+});
+console.log('testando o find', find); //exemplo com arrow function
+
+var arrow = function arrow() {
+  return {
+    nome: 'michael'
+  };
+};
+
+console.log(arrow()); //e o mesmo que
+
+function arrowNormal() {
+  return {
+    nome: 'michael'
+  };
+}
+
+console.log(arrowNormal()); //definir valores padroes para funcoes
+
+function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 4;
+  return a + b;
+}
+
+console.log(soma(2, 5));
+console.log(soma()); //funciona pq foi definido valores padroes
+//desestruturacao de objetos é quando conseguimos pegar variaveis especificos dos objetos
+//ex.:
+
+var usuario = {
+  nome: 'Michael',
+  idade: 26,
+  endereco: {
+    cidade: 'Sorriso',
+    estado: 'MT'
+  }
+};
+var nome = usuario.nome,
+    cidade = usuario.endereco.cidade;
+console.log(nome);
+console.log(cidade); //também funcionam para funcoes
+//ex.:
+
+function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log(nome, idade);
+}
+
+mostraNome(usuario); //operadores rest/spread
+//REST serve para pegar o resto das propriedades
+//https://babeljs.io/docs/en/babel-plugin-proposal-object-rest-spread
+
+var usuario1 = {
+  nome1: 'Michael',
+  idade: 26,
+  empresa: 'Lemontech'
+};
+
+var nome1 = usuario1.nome1,
+    resto = _objectWithoutProperties(usuario1, ["nome1"]);
+
+console.log(nome1);
+console.log(resto); //SPREAD
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3); //template literals 
+
+var nome2 = 'Michael';
+var idade2 = 26;
+console.log("Meu nome \xE9 ".concat(nome2, " e tenho ").concat(idade2, " de idade")); //object short syntax, propriedades com o mesmo nome não precisa reatribuir
+
+var label = 'Label';
+var titulo = 'Titulo';
+var div = {
+  label: label,
+  titulo: titulo,
+  nomeDiv: 'Nome div'
+};
+console.log(div);
--- curso-es6/main.js
+++ curso-es6/main.js
@@ -38,5 +38,160 @@ class Matematica {
 Matematica.soma(10, 20);
 
 
+//tipo de declaracao de varival nao mutavel
+const a = 1;
+
+//só é possivel mudar valores de um objeto declarado como const ex.:
+
+const obj = {nome: 'michael'};
+
+obj.nome = 'Douglas';
+
+console.log(obj);
 
 
+
+//let - variavel definida somente dentro do escopo ex.: 
+
+function teste(x) {
+    let y = 5;
+
+    if (x > 5) {
+        console.log(x,y);
+    }
+}
+
+teste(10);
+
+
+
+
+//arrays
+
+const arr = [1,3,4,8,9,10];
+console.log('novo array', arr);
+
+const newArr = arr.map((item, index) => {
+    return item + index;    
+});
+
+console.log('testando o map', newArr);
+
+const sum = arr.reduce((total, next) => {
+    return total + next;
+});
+
+console.log('testando o reduce', sum);
+
+const filter = arr.filter(item => item % 2 == 0);
+
+console.log('testando o filter', filter);
+
+const find = arr.find(item => {
+    return item === 4;
+});
+
+console.log('testando o find', find);
+
+
+//exemplo com arrow function
+const arrow = () => ({nome: 'michael'});
+console.log(arrow());
+
+//e o mesmo que
+
+function arrowNormal () {
+    return {nome: 'michael'};
+}
+
+console.log(arrowNormal());
+
+
+//definir valores padroes para funcoes
+
+function soma(a = 2,b = 4) {
+    return a + b;
+}
+
+console.log(soma(2,5));
+console.log(soma()); //funciona pq foi definido valores padroes
+
+
+
+//desestruturacao de objetos é quando conseguimos pegar variaveis especificos dos objetos
+//ex.:
+
+const usuario = {
+    nome: 'Michael',
+    idade: 26,
+    endereco: {
+        cidade: 'Sorriso',
+        estado: 'MT',
+    }
+}
+
+const { nome, endereco: {cidade} } = usuario;
+
+console.log(nome);
+console.log(cidade);
+
+//também funcionam para funcoes
+//ex.:
+
+function mostraNome({nome, idade}) {
+    console.log(nome, idade);
+}
+
+mostraNome(usuario);
+
+
+
+//operadores rest/spread
+
+
+//REST serve para pegar o resto das propriedades
+//https://babeljs.io/docs/en/babel-plugin-proposal-object-rest-spread
+
+const usuario1 = {
+    nome1: 'Michael',
+    idade: 26,
+    empresa: 'Lemontech',
+}
+
+
+const {nome1, ...resto } = usuario1;
+
+console.log(nome1);
+console.log(resto);
+
+
+//SPREAD
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+
+
+//template literals 
+
+const nome2 = 'Michael';
+const idade2 = 26;
+
+console.log(`Meu nome é ${nome2} e tenho ${idade2} de idade`);
+
+
+//object short syntax, propriedades com o mesmo nome não precisa reatribuir
+const label = 'Label';
+const titulo = 'Titulo';
+
+const div = {
+    label,
+    titulo,
+    nomeDiv: 'Nome div'
+}
+
+console.log(div);
