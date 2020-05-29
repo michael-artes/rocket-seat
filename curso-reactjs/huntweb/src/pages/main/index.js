0c2d79accb70ca9df40e2d8ad531fd04a8250879
mmoreira <mmoreira.dg@gmail.com>
2020-05-28 20:03:21 -0400

    adicionando eventos nos botoes

--- curso-reactjs/huntweb/src/pages/main/index.js
+++ curso-reactjs/huntweb/src/pages/main/index.js
@@ -8,22 +8,48 @@ export default class Main extends Component {
 
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
 
-        this.setState({products: response.data.docs});
+        const { docs, ...productInfo } = response.data;
+        this.setState({products: docs, productInfo: productInfo, page});
+    };
 
-    }
+    prevPage = () => {
+        const { page } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    };
+
+    nextPage = () => {
+
+        const { page, productInfo } = this.state;
+        
+        console.log(page);
+        console.log(productInfo);
+
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    };
 
     render() {
 
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             <div className="product-list">
@@ -32,9 +58,13 @@ export default class Main extends Component {
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
 
-                        <a href="">Acessar</a>
+                        <a href="/">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         );
     }
