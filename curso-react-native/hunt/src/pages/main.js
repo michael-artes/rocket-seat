5878ddc40d2299219de5fbc35722697835706163
mmoreira <mmoreira.dg@gmail.com>
2020-06-10 22:09:41 -0400

    trabalhando a lista

--- curso-react-native/hunt/src/pages/main.js
+++ curso-react-native/hunt/src/pages/main.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default class Main extends Component {
 
     static navigationOptions = {
         title: 'JSHunt'
+    };
+
+    state = {
+        docs: [],
     };
 
     componentDidMount() {
@@ -20,15 +24,32 @@ export default class Main extends Component {
 
         const { docs } = response.data;
 
-        console.log(docs);
+        this.setState({ docs });
     };
 
+
+    renderItem = ({ item }) => (
+        <View>
+            <Text> {item.title} </Text>
+            <Text> {item.description} </Text>
+            <TouchableOpacity onPress={() =>{}}>
+                <Text> Acessar </Text>
+            </TouchableOpacity>
+        </View>
+    );
 
 
     render() {
         return (
             <View>
                 <Text>Página Main</Text>
+
+                <FlatList
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                />
+
             </View>
         );
     }
